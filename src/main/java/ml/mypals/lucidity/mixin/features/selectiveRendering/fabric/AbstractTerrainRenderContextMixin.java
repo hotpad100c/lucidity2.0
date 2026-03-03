cb8f9af67e22b34264cb52a7348828ee759c50aa
hotpad100c <46704388+hotpad100c@users.noreply.github.com>
2026-03-03 20:17:40 +0900

    B36 move fix

--- src/main/java/ml/mypals/lucidity/mixin/features/selectiveRendering/fabric/AbstractTerrainRenderContextMixin.java
+++ src/main/java/ml/mypals/lucidity/mixin/features/selectiveRendering/fabric/AbstractTerrainRenderContextMixin.java
@@ -9,16 +9,16 @@ import ml.mypals.lucidity.features.selectiveRendering.SelectiveRenderingManager;
 import net.fabricmc.fabric.impl.client.indigo.renderer.render.*;
 
 //? if <=1.21.4 {
-import com.mojang.blaze3d.vertex.VertexConsumer;
-//?} else {
+/*import com.mojang.blaze3d.vertex.VertexConsumer;
+*///?} else {
 //?}
 
 //? if = 1.21.5 {
 /*import net.fabricmc.fabric.api.renderer.v1.material.RenderMaterial;
 *///?}
 //? if >1.21.5 {
-/*import net.minecraft.client.renderer.chunk.ChunkSectionLayer;
-*/
+import net.minecraft.client.renderer.chunk.ChunkSectionLayer;
+
 //?}
 import net.minecraft.client.renderer.RenderType;
 import org.spongepowered.asm.mixin.Mixin;
@@ -35,10 +35,10 @@ import java.util.function.Predicate;
 
 @SuppressWarnings("UnstableApiUsage")
 //? if <=1.21.4 {
-@Mixin(value = AbstractBlockRenderContext.class, remap = false)
-//?} else {
-/*@Mixin(value = AbstractTerrainRenderContext.class, remap = false)
-*///?}
+/*@Mixin(value = AbstractBlockRenderContext.class, remap = false)
+*///?} else {
+@Mixin(value = AbstractTerrainRenderContext.class, remap = false)
+//?}
 public abstract class AbstractTerrainRenderContextMixin
         //? if >=1.21.4 {
         extends AbstractRenderContext
@@ -47,16 +47,16 @@ public abstract class AbstractTerrainRenderContextMixin
     @Final @Shadow(remap = false)
     protected BlockRenderInfo blockInfo;
     //? if <=1.21.5 {
-    @Shadow protected abstract VertexConsumer getVertexConsumer(RenderType renderType);
-    //?} else {
-    /*@Shadow protected abstract VertexConsumer getVertexConsumer(ChunkSectionLayer par1);
-    *///?}
+    /*@Shadow protected abstract VertexConsumer getVertexConsumer(RenderType renderType);
+    *///?} else {
+    @Shadow protected abstract VertexConsumer getVertexConsumer(ChunkSectionLayer par1);
+    //?}
 
     //? if <=1.21.4 {
 
-    //? if <=1.21.3 {
-    /*@Inject(method = "renderQuad", at = @At(value = "INVOKE", target = "Lnet/fabricmc/fabric/impl/client/indigo/renderer/render/AbstractBlockRenderContext;bufferQuad(Lnet/fabricmc/fabric/impl/client/indigo/renderer/mesh/MutableQuadViewImpl;Lcom/mojang/blaze3d/vertex/VertexConsumer;)V"))
-    *///?} else {
+    /*//? if <=1.21.3 {
+    /^@Inject(method = "renderQuad", at = @At(value = "INVOKE", target = "Lnet/fabricmc/fabric/impl/client/indigo/renderer/render/AbstractBlockRenderContext;bufferQuad(Lnet/fabricmc/fabric/impl/client/indigo/renderer/mesh/MutableQuadViewImpl;Lcom/mojang/blaze3d/vertex/VertexConsumer;)V"))
+    ^///?} else {
     @Inject(method = "bufferQuad", at = @At(value = "INVOKE", target = "Lnet/fabricmc/fabric/impl/client/indigo/renderer/render/AbstractBlockRenderContext;bufferQuad(Lnet/fabricmc/fabric/impl/client/indigo/renderer/mesh/MutableQuadViewImpl;Lcom/mojang/blaze3d/vertex/VertexConsumer;)V"))
     //?}
     private void onBufferQuad(MutableQuadViewImpl quad, CallbackInfo ci) {
@@ -67,8 +67,8 @@ public abstract class AbstractTerrainRenderContextMixin
         }
     }
     //? if <=1.21.3 {
-    /*@WrapOperation(method = "renderQuad", at = @At(value = "INVOKE", target = "Lnet/fabricmc/fabric/impl/client/indigo/renderer/render/AbstractBlockRenderContext;bufferQuad(Lnet/fabricmc/fabric/impl/client/indigo/renderer/mesh/MutableQuadViewImpl;Lcom/mojang/blaze3d/vertex/VertexConsumer;)V"))
-    *///?} else {
+    /^@WrapOperation(method = "renderQuad", at = @At(value = "INVOKE", target = "Lnet/fabricmc/fabric/impl/client/indigo/renderer/render/AbstractBlockRenderContext;bufferQuad(Lnet/fabricmc/fabric/impl/client/indigo/renderer/mesh/MutableQuadViewImpl;Lcom/mojang/blaze3d/vertex/VertexConsumer;)V"))
+    ^///?} else {
     @WrapOperation(method = "bufferQuad", at = @At(value = "INVOKE", target = "Lnet/fabricmc/fabric/impl/client/indigo/renderer/render/AbstractBlockRenderContext;bufferQuad(Lnet/fabricmc/fabric/impl/client/indigo/renderer/mesh/MutableQuadViewImpl;Lcom/mojang/blaze3d/vertex/VertexConsumer;)V"))
     //?}
     private void onBufferQuad(AbstractBlockRenderContext instance, MutableQuadViewImpl mutableQuadView, VertexConsumer vertexConsumer, Operation<Void> original) {
@@ -78,25 +78,25 @@ public abstract class AbstractTerrainRenderContextMixin
             original.call(instance,mutableQuadView,vertexConsumer);
         }
     }
-    //?} else {
+    *///?} else {
 
-    /*@Override
+    @Override
     protected void bufferQuad(MutableQuadViewImpl quad, VertexConsumer vertexConsumer) {
         if (!SelectiveRenderingManager.shouldRenderBlock(blockInfo.blockState, blockInfo.blockPos) && !SelectiveRenderingConfigs.isBlockFullyHidden()) {
             for (int i = 0; i < 4; i++) {
                 quad.color(i, rewriteQuadAlpha(quad.color(i), SelectiveRenderingConfigs.HIDDEN_BLOCK_TRANSPARENCY.getIntegerValue()));
             }
             //? if <=1.21.5 {
-            super.bufferQuad(quad, getVertexConsumer(RenderType.translucent()));
-            //?} else {
-            /^super.bufferQuad(quad, getVertexConsumer(ChunkSectionLayer.TRANSLUCENT));
-            ^///?}
+            /*super.bufferQuad(quad, getVertexConsumer(RenderType.translucent()));
+            *///?} else {
+            super.bufferQuad(quad, getVertexConsumer(ChunkSectionLayer.TRANSLUCENT));
+            //?}
         } else {
             super.bufferQuad(quad, vertexConsumer);
         }
     }
 
-    *///?}
+    //?}
 
     @Unique
     private int rewriteQuadAlpha(int color, int alpha) {
